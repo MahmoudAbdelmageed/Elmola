795730900e4e7172ef90ba1e2e07161c072ed710
mahmoud geater <mahmoud.geater3@gmail.com>
2023-08-23 00:27:09 +0300

    skip button

--- lib/views/login/login_screen.dart
+++ lib/views/login/login_screen.dart
@@ -2,6 +2,9 @@ import 'package:el_mola/helper/navigation_class.dart';
 import 'package:el_mola/views/login/registraion_screen.dart';
 import 'package:flutter/material.dart';
 
+import '../../widgets/app_buttons/app_elevated_button.dart';
+import '../../widgets/app_buttons/app_text_button.dart';
+import '../../widgets/app_text.dart';
 import '../home/home_screen.dart';
 
 class LoginPage extends StatelessWidget {
@@ -19,64 +22,84 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('تسجيل الدخول'),
+
+
+
+
       ),
       body: Form(
         key: _formKey,
         child: Padding(
           padding: EdgeInsets.all(16.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(height: 20.0),
-              TextFormField(
-                controller: usernameController,
-                decoration: InputDecoration(
-                  labelText: 'اسم المستخدم',
+              Align(
+                alignment: Alignment.topLeft,
+                child: AppTextButton(
+                  onPressed: (){
+                    NavigationClass.finalNavigate(context, HomeScreen());
+                  },
+                  text: "Skip",
+                  textColor: Colors.black,
+                  backgroundColor: Colors.white,
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'ادخل اسم المستخدم';
-                  }
-                  return null;
-                },
               ),
-              SizedBox(height: 10.0),
-              TextFormField(
-                controller: passwordController,
-                decoration: InputDecoration(
-                  labelText: 'الباسورد',
-                ),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'ادخل الباسورد';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    if (usernameController.text == "mahmoud" &&
-                        passwordController.text == "123456789") {
-                      NavigationClass.navigate(context, HomeScreen());
-                    }else{
-                      showSnackBarHelper(context,"اسم المستخدم او الباسورد خطأ");
-                    }
-                  }
-                },
-                style: ButtonStyle(
-                    fixedSize: MaterialStateProperty.all(
-                        Size(MediaQuery.of(context).size.width, 50))),
-                child: Text('سجل الدخول', style: TextStyle(fontSize: 20)),
-              ),
-              SizedBox(height: 10.0),
-              TextButton(
-                onPressed: () {
-                  NavigationClass.navigate(context, RegistrationPage());
-                },
-                child: Text('مستخدم جديد', style: TextStyle(fontSize: 24)),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    controller: usernameController,
+                    decoration: InputDecoration(
+                      labelText: 'اسم المستخدم',
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'ادخل اسم المستخدم';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 10.0),
+                  TextFormField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'الباسورد',
+                    ),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'ادخل الباسورد';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 20.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        if (usernameController.text == "mahmoud" &&
+                            passwordController.text == "123456789") {
+                          NavigationClass.navigate(context, HomeScreen());
+                        }else{
+                          showSnackBarHelper(context,"اسم المستخدم او الباسورد خطأ");
+                        }
+                      }
+                    },
+                    style: ButtonStyle(
+                        fixedSize: MaterialStateProperty.all(
+                            Size(MediaQuery.of(context).size.width, 50))),
+                    child: Text('سجل الدخول', style: TextStyle(fontSize: 20)),
+                  ),
+                  SizedBox(height: 10.0),
+                  TextButton(
+                    onPressed: () {
+                      NavigationClass.navigate(context, RegistrationPage());
+                    },
+                    child: Text('مستخدم جديد', style: TextStyle(fontSize: 24)),
+                  ),
+                ],
               ),
             ],
           ),
